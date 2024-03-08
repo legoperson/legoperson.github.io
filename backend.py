7cfc0c5e68c4b181959fe7bf58a64c1b5d5bd624
legoperson <64126997+legoperson@users.noreply.github.com>
2024-03-08 21:30:14 +1100

    Update backend.py

--- backend.py
+++ backend.py
@@ -6,14 +6,14 @@ data = pd.read_csv('graph data.csv')
 df = pd.DataFrame(data)
 st.write(df)
 # 在Streamlit应用中创建一个下拉菜单，用于选择一个名字
-#selected_name = st.selectbox('Select a property:', df['properties'])
+selected_name = st.selectbox('Select a property:', df['properties'])
 
 # 在数据框中找到选择的名字对应的行
-#selected_row = df[df['properties'] == selected_name]
+selected_row = df[df['properties'] == selected_name]
 
 # 显示相应行等于1的列
-#st.write(f"Property with the name {selected_name} is similar to these properties in rental price:")
-#st.write(selected_row.loc[:, selected_row.eq(1).any()])
+st.write(f"Property with the name {selected_name} is similar to these properties in rental price:")
+st.write(selected_row.loc[:, selected_row.eq(1).any()])
 
 
  
